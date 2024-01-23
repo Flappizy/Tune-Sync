21a17a2638c2d9c81500b1eddbeaef1ebfb50cc9
Flappizy <75231665+Flappizy@users.noreply.github.com>
2024-01-23 13:43:48 +0100

    Delete dist directory

--- dist/Domain/Enums/authProvider.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var AuthProvider;
-(function (AuthProvider) {
-    AuthProvider[AuthProvider["Email"] = 0] = "Email";
-})(AuthProvider || (AuthProvider = {}));
-//# sourceMappingURL=authProvider.js.map
--- dist/Infrastructure/Utilities/cookieHandler.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=cookieHandler.js.map
